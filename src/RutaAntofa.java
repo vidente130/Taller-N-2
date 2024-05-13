f7a2f23560860e3b7e68af395f825e53dfe9ed70
renato aguilar <renatoaguilarr@gmail.com>
2024-05-12 20:54:21 -0400

    actualizacion de la clase RutaAntofa
     se crea un scanner y se crea la funcion estaEnRuta para buscar en las listas

--- src/RutaAntofa.java
+++ src/RutaAntofa.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class RutaAntofa {
+   Scanner sc = new Scanner(System.in);
+
     private ArrayList<String> rutaA;
 
     private ArrayList<String> rutaB;
@@ -14,6 +17,7 @@ public class RutaAntofa {
         this.rutaB = new ArrayList<>();
         this.rutaC = new ArrayList<>();
         this.rutaD = new ArrayList<>();
+        //RUTA A
         rutaA.add("UNIVERSIDAD DE ANTOFAGASTA");
         rutaA.add("COVIEFI");
         rutaA.add("UNIVERSIDAD CATOLICA DEL NORTE");
@@ -22,12 +26,14 @@ public class RutaAntofa {
         rutaA.add("MALL PLAZA");
         rutaA.add("MUNICIPALIDAD");
         rutaA.add("TERMINAL DE BUSES");
+        //RUTA B
         rutaB.add("UNIVERSIDAD DE ANTOFAGASTA");
         rutaB.add("UNIVERSIDAD CATOLICA DEL NORTE");
         rutaB.add("PLAYA BALNEARIO");
         rutaB.add("PLAZA COLON");
         rutaB.add("AVENIDA SALVADOR ALLENDE");
         rutaB.add("HOSPITAL REGIONAL");
+        //RUTA C
         rutaC.add("COVIEFI");
         rutaC.add("ENJOY");
         rutaC.add("COSTANERA");
@@ -36,6 +42,7 @@ public class RutaAntofa {
         rutaC.add("HOSPITAL ANTIGUO");
         rutaC.add("CEMENTERIO REGIONAL");
         rutaC.add("MERCADO CENTRAL");
+        //RUTA D
         rutaD.add("UNIVERSIDAD CATOLICA DEL NORTE");
         rutaD.add("PLAZA JARDIN JAPONES");
         rutaD.add("JUMBO NORTE");
@@ -45,5 +52,73 @@ public class RutaAntofa {
         rutaD.add("TERMINAL DE BUSES");
         rutaD.add("PLAZA BICENTENARIA");
 
+    }
+
+
+    public void  estaEnRuta(){
+        System.out.println("Por que ruta desea ir");
+        String tipoRuta = sc.nextLine();
+        System.out.println("A que localidad desea ir");
+        String localidad = sc.nextLine();
+
+        estaEnRuta(localidad,tipoRuta);
+    }
+
+    public boolean estaEnRuta(String localidad, String tipoRuta){
+        
+        if (tipoRuta.equalsIgnoreCase("a") ){
+
+            if( rutaA.contains(localidad)){
+                System.out.println("La localidad a la que desea se encuentra en esta ruta");
+                return true;
+
+            }
+            else {
+                System.out.println("La localidad a la que desea NO se encuentra en esta ruta");
+                return false;
+            }
+
+        }
+        if (tipoRuta.equalsIgnoreCase("b") ){
+
+            if(rutaB.contains(localidad)){
+                System.out.println("La localidad a la que desea se encuentra en esta ruta");
+                return true;
+
+            }
+            else {
+                System.out.println("La localidad a la que desea NO se encuentra en esta ruta");
+                return false;
+            }
+
+        }
+        if (tipoRuta.equalsIgnoreCase("c") ){
+
+            if (rutaC.contains(localidad)){
+                System.out.println("La localidad a la que desea se encuentra en esta ruta");
+                return true;
+
+            }
+            else {
+                System.out.println("La localidad a la que desea NO se encuentra en esta ruta");
+                return false;
+            }
+
+        }
+        if (tipoRuta.equalsIgnoreCase("d") ){
+
+            if (rutaD.contains(localidad)){
+                System.out.println("La localidad a la que desea se encuentra en esta ruta");
+                return true;
+
+            }
+            else {
+                System.out.println("La localidad a la que desea NO se encuentra en esta ruta");
+                return false;
+            }
+
+        }
+
+        return false;
     }
 }
